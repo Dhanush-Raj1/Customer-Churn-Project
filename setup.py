e6989d29cdbe4d6f42b4e3053a87debd3285b250
Dhanush <dhanush1004raj@gmail.com>
2024-05-16 14:30:32 +0530

    2nd upload requirements & setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+
+setup(name = 'Customer-Churn-Project',
+      version = '0.0.1',
+      author = 'Dhanush Raj',
+      author_email ='dhanushlogan1004@gmail.com',
+      packages = find_packages(),
+      install_requires = ['pandas', 'numpy', 'matplotlib', 'seaborn', 'plotly']
+
+)
