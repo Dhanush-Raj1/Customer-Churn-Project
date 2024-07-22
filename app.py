872e336dedae9cdf3c4eff69f69544a36d0437a8
Dhanush <dhanush1004raj@gmail.com>
2024-07-22 13:31:05 +0530

    modified app, model, preprocessor

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+from src.logger import logging
 from flask import Flask, request, render_template
 from sklearn.preprocessing import StandardScaler
 
@@ -9,44 +10,56 @@ application = Flask(__name__)
 app = application
 
 
-# route for home page
+# route for home page   
 @app.route('/')
-def index():
+def home():
     return render_template('home_page.html')
 
 
+# route for predict page
 @app.route('/predict', methods=['GET', 'POST'])
-def predict_newdata():
+def predict():
+    
     if request.method == 'GET':
-        return render_template('home_page.html')
+        return render_template('predict_page.html')
+    
+    
     else:
-        data = NewData( gender = request.form.get('gender'),
-                        SeniorCitizen = request.form.get('SeniorCitizen'), 
-                        Partner = request.form.get('Partner'), 
-                        Dependents = request.form.get('Dependents'), 
-                        tenure = float(request.form.get('tenure')), 
-                        PhoneService = request.form.get('PhoneService'), 
-                        MultipleLines = request.form.get('MultipleLines'), 
-                        InternetService = request.form.get('InternetService'), 
-                        OnlineSecurity = request.form.get('OnlineSecurity'), 
-                        OnlineBackup = request.form.get('OnlineBackup'), 
-                        DeviceProtection = request.form.get('DeviceProtection'), 
-                        TechSupport = request.form.get('TechSupport'), 
-                        StreamingTV = request.form.get('StreamingTV'), 
-                        StreamingMovies = request.form.get('StreamingMovies'), 
-                        Contract = request.form.get('Contact'), 
-                        PaperlessBilling = request.form.get('PaperlessBilling'), 
-                        PaymentMethod = request.form.get('PaymentMethod'), 
-                        MonthlyCharges = float(request.form.get('MonthlyCharges')), 
-                        TotalCharges = float(request.form.get('TotalCharges'))   )
+        try:
+            logging.info("POST Request has been made.")
+            data = NewData( gender = request.form.get('Gender'),
+                            SeniorCitizen = request.form.get('Senior Citizen'), 
+                            Partner = request.form.get('Partner'), 
+                            Dependents = request.form.get('Dependents'), 
+                            tenure = float(request.form.get('Tenure (months)')), 
+                            PhoneService = request.form.get('Phone Service'), 
+                            MultipleLines = request.form.get('Multiple Lines'), 
+                            InternetService = request.form.get('Internet Service'), 
+                            OnlineSecurity = request.form.get('Online Security'), 
+                            OnlineBackup = request.form.get('Online Backup'), 
+                            DeviceProtection = request.form.get('Device Protection'), 
+                            TechSupport = request.form.get('Tech Support'), 
+                            StreamingTV = request.form.get('Streaming TV'), 
+                            StreamingMovies = request.form.get('Streaming Movies'), 
+                            Contract = request.form.get('Contract Type'), 
+                            PaperlessBilling = request.form.get('Paperless Billing'), 
+                            PaymentMethod = request.form.get('Payment Method'), 
+                            MonthlyCharges = float(request.form.get('Monthly Charges', 0)), 
+                            TotalCharges = float(request.form.get('Total Charges', 0))   )
+            
+            df = data.get_data_as_dataframe()
+            logging.info("Data has been converted to a DataFrame")
+            print(df)
+            
+            prediction = Predict()
+            logging.info("Predicting process has been initialized.")
+            results = prediction.predict_data(df)
+            print("Prediction results:", results)
+            
+            return render_template('predict_page.html', results=results)
         
-        newdata_df = data.get_data_as_dataframe()
-        print(newdata_df)
-        
-        prediction = Predict()
-        results = prediction.predict_data(newdata_df)
-        
-        return render_template('home.html', results=results[0])
+        except ValueError as e:
+            return render_template('predict_page.html', error=str(e))
     
 
 if __name__ == "__main__":
